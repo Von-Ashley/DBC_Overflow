5390c3166a3b9799f525f011daa518758c28c68c
Ashley Humphrey <ashley.humphrey2@yahoo.com>
2016-11-27 22:09:47 -0800

    Adds question form and answer form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,12 +3,43 @@ get '/questions' do
   erb :'/questions/index'
 end
 
+
+
+get '/questions/new' do
+  if logged_in?
+    erb :'/questions/new'
+  else
+    # alert("Please you have to login to post a question")
+    redirect to '/questions'
+  end
+
+end
+
+post '/questions' do
+    @question = Question.new(params[:question])
+  if @question.save
+    redirect to '/questions'
+  else
+    erb :'questions/new'
+  end
+
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @comments = @question.comments
   @answers = @question.answers
   
-
   erb :'/questions/show'
-
 end
+
+post '/answers/:id' do
+  @question = Question.find(params[:id])
+  params[:answer][:answer]
+  @question.answers.create(user_id: session[:user_id], answer: params[:answer][:answer])
+
+  redirect to "/questions/#{@question.id}"
+end
+
+
+
--- db/migrate/20161126123828_create_answers.rb
+++ db/migrate/20161126123828_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration
     create_table :answers do |t|
       t.integer :user_id
       t.integer :question_id
-      t.string :answer, :null => false
+      t.string :answer, null: false
 
       t.timestamps null: false
     end
